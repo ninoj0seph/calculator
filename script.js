a2d1912ca16d245e2315e07a1bb4ff8e0d4af721
ninoj0seph <ninojoseph.tugade@gmail.com>
2017-03-09 09:27:18 -0800

    stable v.03

--- script.js
+++ script.js
@@ -1,60 +1,104 @@
-var container = [];
+//Console.logs are available and present for demonstration and review purposes but will be deleted on final release
+var reset = new clearConstructor();
+var click = new clickConstructor();
 $(document).ready(clickHandler);
 
 function clickHandler() {
-    $(".numberButtonsContainer").on('click', 'div', numberClicked);
-    $(".operatorButtonContainer").on('click', 'div', operatorClicked);
+    $(".numberButtonsContainer").on('click', 'div', function (){
+        click.numberClicked($(this).text());
+    });
+    $(".operatorButtonContainer").on('click', 'div', function () {
+        click.operatorClicked($(this).text());
+    });
     $(".clearButtonsContainer").on('click', 'div', function () {
         reset.clear($(this).text());
     });
 }
 
-function numberClicked(){
-    var number = $(this).text();
-    if(number === "="){
-        equalSignClicked();
-        return;
-    } else if(isNaN(container[container.length-1])){
-        container.push(number);
-        $(".displayBox").append(container[container.length -1]);
-    } else {
-        container[container.length - 1] += (number);
-    }
-    $(".displayBox").text(container[container.length -1]);
-    console.log(container);
-}
+function clickConstructor() {
+    this.container = [];
+    this.decimal = false;
+    this.numberClicked = function (number) {
+        if(number === "="){
+            this.equalSignClicked();
+            return;
+        } else if(number === "."){
+            this.decimalClicked();
+            return;
+        } else if(!isNaN(this.container[this.container.length-1]) || this.container[this.container.length - 1] === "."){
+            this.container[this.container.length - 1] += (number);
+        } else {
+            this.container.push(number);
+            //$(".displayBox").append(this.container[this.container.length -1]);
+        }
+        $(".displayBox").text(this.container[this.container.length -1]);
+        console.log(this.container);
+    };
 
-function operatorClicked(){
-    var operator = $(this).text();
-    if(!isNaN(container[container.length-1])){
-        container.push(operator);
-    } else {
-        container[container.length - 1] = operator;
-    }
-    console.log(container);
-}
+    this.operatorClicked = function (operator) {
+        this.decimal = false;
+        if(!isNaN(this.container[this.container.length-1])){
+            this.container.push(operator);
+        } else {
+            this.container[this.container.length - 1] = operator;
+        }
+        console.log(this.container);
+    };
 
-function equalSignClicked(){
-    for(i = 0; i < container.length; i++){
-        if(!isNaN(container[i])){
-            container[i] = parseFloat(container[i]);
+    this.equalSignClicked = function () {
+        console.log("=");
+        operator.findAndOperate(this.container);
+    };
+
+    this.decimalClicked = function () {
+        if(this.decimal === false){
+            if(isNaN(this.container[this.container.length-1])){
+                this.container.push(".");
+                this.decimal = true;
+            } else {
+                this.container[this.container.length - 1] += (".");
+                this.decimal = true;
+            }
+            console.log(this.container);
         }
     }
-    operator.findAndOperate(container);
 }
 
 var operator = {
     findAndOperate : function (arrayInput) {
-        for(var x = 0; x < arrayInput.length; x++){
-            // try using find
-            if(arrayInput[x] === "+" || arrayInput[x] === "-" || arrayInput[x] === "X" || arrayInput[x] === "÷"){
-                arrayInput[0] = this[arrayInput[x]](arrayInput[x-1],arrayInput[x+1]);
-                arrayInput.splice(1,arrayInput.length);
-                console.log(arrayInput);
-                $('.displayBox').text(arrayInput[0]);
-            }
+        this.calculate = function () {
+            arrayInput[this.index] = this[arrayInput[this.index]](parseFloat(arrayInput[this.index-1]),parseFloat(arrayInput[this.index+1]));
+            arrayInput.splice(this.index - 1,1);
+            arrayInput.splice(this.index,1);
+        };
+
+        this.index = arrayInput.indexOf("X");
+        while (this.index !== -1){
+            this.calculate();
+            this.index = arrayInput.indexOf("X");
         }
+
+        this.index = arrayInput.indexOf("÷");
+        while (this.index !== -1){
+            this.calculate();
+            this.index = arrayInput.indexOf("÷");
+        }
+
+        this.index = arrayInput.indexOf("+");
+        while (this.index !== -1){
+            this.calculate();
+            this.index = arrayInput.indexOf("+");
+        }
+
+        this.index = arrayInput.indexOf("-");
+        while (this.index !== -1){
+            this.calculate();
+            this.index = arrayInput.indexOf("-");
+        }
+        console.log(arrayInput[0]);
+        $('.displayBox').text(arrayInput[0]);
     },
+
     "+" : function (numberOne, numberTwo) {
         return numberOne + numberTwo;
     },
@@ -65,24 +109,28 @@ var operator = {
         return numberOne * numberTwo;
     },
     "÷" : function (numberOne, numberTwo) {
-        return numberOne / numberTwo;
+        return numberOne === 1 && numberTwo === 0 ? "Err0r!" : numberOne / numberTwo;
     }
 };
 
-var reset = {
-    "clear" : function (type) {
-        this[type]();
-        console.log(container);
-    },
-
-    "CE" : function () {
-        container = [];
+function clearConstructor() {
+    this.clear = function (type) {
+            this[type]();
+            console.log(click.container);
+        };
+    this.CE = function () {
+        click.container = [];
+        click.decimal = false;
         $(".displayBox").text("Cleared");
-    },
-    "C" : function () {
-        if(container.length > 0) {
-            container[container.length - 1] = "";
+        setTimeout(function (){
+            $(".displayBox").text("");
+        },100);
+    };
+    this.C = function () {
+        if(!isNaN(click.container[click.container.length - 1]) && click.container.length > 0 ) {
+            click.decimal = false;
+            click.container[click.container.length - 1] = "";
             $(".displayBox").text("");
         }
-    }
-};
+    };
+}
